470f7dbf4ccc5511e7787781cbde995fc56a173b
Jusna <ec2-user@ip-172-31-7-78.ap-northeast-1.compute.internal>
2024-04-28 05:33:23 +0000

    [update]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
-    
-  end 
-  
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
   def destroy
-  end 
-  
+   BookComment.find(params[:id]).destroy
+   redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
 end
