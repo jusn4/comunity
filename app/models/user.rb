a3fb98db977baf249909fdc13b4d6078ee2afee2
Jusna <ec2-user@ip-172-31-7-78.ap-northeast-1.compute.internal>
2024-04-28 09:30:15 +0000

    [Add]relationship_cont,model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
   validates :name, length: { in: 2..20 }, uniqueness: true, presence: true
   validates :introduction, length: {maximum: 50 }
@@ -16,5 +21,20 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  #指定したユーザーをフォローする
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  #指定したユーザーをフォロー解除する
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  #指定したユーザーをフォローしているかどうか判定する
+  def following?(user)
+    followings.include?(user)
   end
 end
