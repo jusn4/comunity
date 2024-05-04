eb44649492208eef717e654c7f161a89238a5877
jusna <164776044+jusn4@users.noreply.github.com>
2024-05-04 01:42:37 +0000

    [Add]gUser_cont

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,2 +1,16 @@
 class GroupUsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    group_user = current_user.group_user.new(group_id: params[:group_id])
+    group_user.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    group_user = current_user.group_user.find_by(group_id: params[:group_id])
+    group_user.destroy
+    redirect_to request.referer
+  end
+
 end
