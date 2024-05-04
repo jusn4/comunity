e4c40fefbe6444882be88ba175332fb586a1f500
jusna <164776044+jusn4@users.noreply.github.com>
2024-05-04 02:13:52 +0000

    [Add]join-btn

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -2,13 +2,13 @@ class GroupUsersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    group_user = current_user.group_user.new(group_id: params[:group_id])
+    group_user = current_user.group_users.new(group_id: params[:group_id])
     group_user.save
     redirect_to request.referer
   end
 
   def destroy
-    group_user = current_user.group_user.find_by(group_id: params[:group_id])
+    group_user = current_user.group_users.find_by(group_id: params[:group_id])
     group_user.destroy
     redirect_to request.referer
   end
