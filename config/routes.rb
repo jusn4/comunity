43c6f4fec95124c4471d846542c5a259ad934f23
jusna <164776044+jusn4@users.noreply.github.com>
2024-05-04 01:35:51 +0000

    [Add]gUser

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
       get "followers" => "relationships#followers", as: "followers"
   end
 
-  resources :groups, only: [:new, :index, :show, :edit, :update,:create]
+  resources :groups, only: [:new, :index, :show, :edit, :update,:create] do
+    resource :group_user, only: [:create, :destroy]
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
